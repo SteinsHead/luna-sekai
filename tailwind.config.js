061cb590b5a58e7355c050ff2ffee1da26f0deae
christina <headsteins@gmail.com>
2023-05-25 23:24:17 +0800

    添加tailwindcss失败

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, jsx, ts, tsx}"],
+  content: ['./src/**/*.{html, js, jsx, ts, tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
